09ba6690a0e510c72cacb0e2293c87f86299ea1f
Andrey G <gav27@mail.ru>
2022-10-02 22:43:34 +0500

    change in task

--- 2/2hw.py
+++ 2/2hw.py
@@ -1,8 +1,8 @@
 #Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
 num=int(input('Введите число N: '))
-rez=[1]
+rez=[]
 x=1
-for i in range(1,num):
-    x+=x*i
+for i in range(1,num+1):
+    x*=i
     rez.append(x)
 print(rez)
